e8969ddcd764ee1c15639f0ef8a7c7b6b2c647b2
Yohan Belval <yohan.belval@gsoft.com>
2019-09-17 22:43:01 -0400

    --wip-- [skip ci]

---
+++ src/Unit-1/DoThis/WintTail/ConsoleWriterActor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Akka.Actor;
+
+namespace WinTail
+{
+    /// <summary>
+    /// Actor responsible for serializing message writes to the console.
+    /// (write one message at a time, champ :)
+    /// </summary>
+    class ConsoleWriterActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case Messages.InputError inputError:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(inputError.Reason);
+                    break;
+                
+                case Messages.InputSuccess inputSuccess:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(inputSuccess.Reason);
+                    break;
+                
+                default:
+                    Console.WriteLine(message);
+                    break;
+            }
+            
+            Console.ResetColor();
+        }
+    }
+}
---
+++ src/Unit-1/DoThis/WintTail/Program.cs
@@ -0,0 +1,27 @@
+﻿using Akka.Actor;
+
+namespace WinTail
+{
+    #region Program
+    class Program
+    {
+        public static ActorSystem MyActorSystem;
+
+        static void Main(string[] args)
+        {
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
+
+            var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() =>
+                new ConsoleWriterActor()));
+            var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() =>
+                new ConsoleReaderActor(consoleWriterActor)));
+
+
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+            // blocks the main thread from exiting until the actor system is shut down
+            MyActorSystem.WhenTerminated.Wait();
+        }
+    }
+    #endregion
+}
